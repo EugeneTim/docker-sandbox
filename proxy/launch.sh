14b7ddfef565f098dedb41bf0979cbef04cc97ba
Eugene <eugene.timmy@gmail.com>
2017-10-31 15:52:30 +0300

    Correct launch.sh

--- proxy/launch.sh
+++ proxy/launch.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-docker run -d --name http-server-two eugene/nginx-web2
-docker run -d --name http-server-one eugene/nginx-web1
-docker run -d -P --name http-proxy --link http-server-two:http-server-two  --link http-server-one:http-server-one eugene/nginx-proxy
+docker run -itd --name http-server-two eugene/nginx-web2
+docker run -itd --name http-server-one eugene/nginx-web1
+docker run -itd -P --name http-proxy --link http-server-two:http-server-two  --link http-server-one:http-server-one eugene/nginx-proxy
